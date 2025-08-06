2fc5074550d99958a6780e9fc62fea56e8db4d27
Eduardo Lago <edulago173@gmail.com>
2025-08-06 07:00:15 -0300

    Configuração do EF Core: criação dos models, relacionamentos e conexão com o DB

---
+++ EFVendas/Banco/Context.cs
@@ -0,0 +1,32 @@
+﻿using EFVendas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFVendas.Banco;
+
+internal class Context : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Pedido> pedidos { get; set; }
+    public DbSet<PedidoItem> pedidoItems { get; set; }
+    public DbSet<Produto> Produto { get; set; }
+
+    private string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFVendas;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PedidoItem>()
+            .HasOne(p => p.Pedido)
+            .WithMany(p => p.PedidoItens) 
+            .HasForeignKey(p => p.PedidoId);
+
+        modelBuilder.Entity<PedidoItem>()
+            .HasOne(p => p.Produto)
+            .WithMany(p => p.PedidoItens)
+            .HasForeignKey(p => p.PedidoId);
+    }
+}
---
+++ EFVendas/Models/Pedido.cs
@@ -0,0 +1,10 @@
+﻿namespace EFVendas.Models;
+
+public class Pedido
+{
+    public int Id { get; set; }
+    public DateTime Data { get; set; }
+    public int ClienteId { get; set; }
+    public Cliente Cliente { get; set; }
+    public ICollection<PedidoItem> PedidoItems { get; set; }
+}
